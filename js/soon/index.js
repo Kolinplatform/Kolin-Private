77968e65f0e415df765976a7dc3c1ef6594d5c32
Felipe A. Millacura <41435710+millacurafa@users.noreply.github.com>
2020-03-21 19:17:47 +0000

    replace old and incorrect files

--- js/soon/index.js
+++ js/soon/index.js
@@ -1,7 +1,7 @@
 const countdown = document.querySelector('.countdown');
 
 // Set Launch Date (ms)
-const launchDate = new Date('Mar 31, 2019 13:00:00').getTime();
+const launchDate = new Date('Apr 1, 2019 00:00:00').getTime();
 
 // Update every second
 const intvl = setInterval(() => {
